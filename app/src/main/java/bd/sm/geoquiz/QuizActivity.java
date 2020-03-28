e368dc138e11245eca560ea2742ec4365198c264
Hackertag <hakertaggroup@gmail.com>
2020-03-28 14:09:27 +0600

    Chapter 3
    Update  app in 3rd level

--- app/src/main/java/bd/sm/geoquiz/QuizActivity.java
+++ app/src/main/java/bd/sm/geoquiz/QuizActivity.java
@@ -1,20 +1,25 @@
 package bd.sm.geoquiz;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class QuizActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
-    private ImageButton mNextButton;
-    private ImageButton mPreviousButton;
+    private Button mNextButton;
+    private Button mPreviousButton;
     private TextView mQuestionTextView;
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
+   /* public static int d(String tag, String msg) {
+        return 0;
+    }*/
+
     private Question[] mQuestionsBank = new Question[]{
             new Question(R.string.question_bangladesh, true),
             new Question(R.string.question_australia, true),
@@ -29,18 +34,21 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
         // Set text question on first question on zero index
         mQuestionTextView = findViewById(R.id.question_text_view);
         /*int question=mQuestionsBank[mCurrentIndex].getTextResId();
         mQuestionTextView.setText(question);*/
-        updateQuestion();
-
         mTrueButton = findViewById(R.id.true_button);
         mFalseButton = findViewById(R.id.false_button);
         mPreviousButton = findViewById(R.id.previous_button);
         mNextButton = findViewById(R.id.next_button);
 
+        if (savedInstanceState !=null)
+            mCurrentIndex=savedInstanceState.getInt(KEY_INDEX,0);
+
+        updateQuestion();
         mPreviousButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -82,8 +90,16 @@ public class QuizActivity extends AppCompatActivity {
     }
 
     private void updateQuestion() {
-        int question = mQuestionsBank[mCurrentIndex].getTextResId();
-        mQuestionTextView.setText(question);
+        // Log a message at "debug" log level
+        Log.d(TAG, "Current question index: " + mCurrentIndex);
+        try {
+            int question = mQuestionsBank[mCurrentIndex].getTextResId();
+            mQuestionTextView.setText(question);
+        }catch (ArrayIndexOutOfBoundsException ex){
+            // Log a message at "error" log level, along with an exception stack trace
+            Log.e(TAG, "Index was out of bounds", ex);
+        }
+
     }
 
     private void checkAnswer(boolean userPressedTrue) {
@@ -96,5 +112,43 @@ public class QuizActivity extends AppCompatActivity {
         }
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart(Bundle) called");
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume(Bundle) called");
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause(Bundle) called");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle saveInstanceState) {
+        super.onSaveInstanceState(saveInstanceState);
+        Log.i(TAG,"onSaveInstanceState called");
+        saveInstanceState.putInt(KEY_INDEX,mCurrentIndex);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop(Bundle) called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy(Bundle) called");
+    }
+
+
+
 
 }
